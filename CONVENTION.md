b544628fb4e77d4533b05b383f73a3ae5ce441b8
Martin Simengård <summengardin@gmail.com>
2022-03-03 15:44:09 +0100

    Started on CONVENTION.md

---
+++ CONVENTION.md
@@ -0,0 +1,6 @@
+#Coding convention
+
+Variable name: camelCase \
+Class member name: _camelCase \
+Class name: UpperCamelCase \
+Constant: ALL_CAPS 
---
+++ src/Person.hpp
@@ -0,0 +1,17 @@
+//
+// Created by simen on 3/3/2022.
+//
+
+#ifndef FORFEDREDIAGRAM_PERSON_HPP
+#define FORFEDREDIAGRAM_PERSON_HPP
+
+// TODO Her må det lages en klasse
+
+class Person{
+public:
+
+private:
+    _firstName
+};
+
+#endif //FORFEDREDIAGRAM_PERSON_HPP
