0b91e49a3a2b34a940e6cc05d395f6bce3c1f729
MartinSimengard <simengardmartin@gmail.com>
2022-03-15 13:33:49 +0100

    First test is ran

---
+++ test/test_main.cpp
@@ -0,0 +1,6 @@
+//
+// Created by Martin on 15.03.2022.
+//
+
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
---
+++ test/test_person.cpp
@@ -0,0 +1,15 @@
+//
+// Created by Martin on 15.03.2022.
+//
+
+
+#include "catch.hpp"
+
+TEST_CASE("Learning in progress"){
+    int i = 0;
+    int j = 1;
+    int k = 0;
+
+    REQUIRE(i == j);
+    REQUIRE(i == k);
+}
